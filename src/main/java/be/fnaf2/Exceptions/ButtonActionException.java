66227a481c4bec575ce674f8e773fa2d3405de2a
Bent <bentvdw@gmail.ccom>
2024-03-20 22:45:07 +0100

    BattleshipsPresenter iets wa veranderd. trust.

---
+++ src/main/java/be/fnaf2/Exceptions/ButtonActionException.java
@@ -0,0 +1,7 @@
+package be.fnaf2.Exceptions;
+
+public class ButtonActionException extends Exception {
+    public ButtonActionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/be/fnaf2/view/main/BattleshipsPresenter.java
+++ src/main/java/be/fnaf2/view/main/BattleshipsPresenter.java
@@ -1,6 +1,7 @@
 // BattleshipsPresenter.java
 package be.fnaf2.view.main;
 
+import be.fnaf2.Exceptions.ButtonActionException;
 import be.fnaf2.Exceptions.ButtonInitializationException;
 import be.fnaf2.model.GridModel;
 import be.fnaf2.view.rules.RulesPresenter;
@@ -9,14 +10,14 @@ import be.fnaf2.view.settings.SettingsPresenter;
 import be.fnaf2.view.gridplacement.GridPresenter;
 import be.fnaf2.view.gridplacement.Gridview;
 import be.fnaf2.view.settings.SettingsView;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class BattleshipsPresenter {
     private final BattleshipsView view;
@@ -30,16 +31,16 @@ public class BattleshipsPresenter {
         setButtonAction(view.getSingleplayerButton(), "Singleplayer");
         setButtonAction(view.getMultiplayerButton(), "Multiplayer");
         setButtonAction(view.getSettingButton(), "Settings");
-        setButtonAction(view.getRulesButton(), "rules");
+        setButtonAction(view.getRulesButton(), "Rules");
     }
 
     private void setButtonAction(Button button, String gameMode) {
         if (button != null) {
             button.setOnAction(event -> {
-                if (gameMode.equalsIgnoreCase("rules")) {
-                    switchToRulesView(); // Schakel over naar de RulesView als de Rules-knop wordt ingedrukt
-                } else {
-                    showConfirmationAlert(gameMode);
+                try {
+                    handleButtonAction(button, gameMode);
+                } catch (ButtonActionException e) {
+                    e.printStackTrace();
                 }
             });
         } else {
@@ -47,21 +48,23 @@ public class BattleshipsPresenter {
         }
     }
 
-    private void showConfirmationAlert(String gameMode) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation");
-        alert.setHeaderText("Switching to " + gameMode + " mode");
-        alert.setContentText("Are you sure you want to switch to " + gameMode + " mode?");
-
-        alert.showAndWait().ifPresent(response -> {
-            if (gameMode.equalsIgnoreCase("settings")) {
+    private void handleButtonAction(Button button, String gameMode) throws ButtonActionException {
+        switch (gameMode.toLowerCase()) {
+            case "rules":
+                switchToRulesView();
+                break;
+            case "settings":
                 switchToSettingsView();
-            } else if (gameMode.equalsIgnoreCase("multiplayer")) {
+                break;
+            case "multiplayer":
                 switchToMultiPlayerView();
-            }else if (gameMode.equalsIgnoreCase("singleplayer")) {
+                break;
+            case "singleplayer":
                 switchToSinglePlayerView();
-            }
-        });
+                break;
+            default:
+                throw new ButtonActionException("Unsupported game mode: " + gameMode);
+        }
     }
 
     private void switchToSettingsView() {
@@ -69,7 +72,8 @@ public class BattleshipsPresenter {
         new SettingsPresenter(settingsView);
         view.getScene().setRoot(settingsView);
     }
-    private void switchToRulesView(){
+
+    private void switchToRulesView() {
         RulesView rulesView = new RulesView();
         new RulesPresenter(rulesView);
         view.getScene().setRoot(rulesView);
@@ -79,39 +83,48 @@ public class BattleshipsPresenter {
         Stage stage = (Stage) view.getScene().getWindow();
         Gridview gridview = new Gridview(stage);
         new GridPresenter(new GridModel(), gridview);
-        HBox hbox = new HBox(gridview.getShipTypeChoiceBox(), gridview.getClearButton(), gridview.getUndoButton(),gridview.getNextButton());
+        HBox hbox = new HBox(gridview.getShipTypeChoiceBox(), gridview.getClearButton(), gridview.getUndoButton(), gridview.getNextButton());
         hbox.setSpacing(10);
-        // To add some space between the buttons
         VBox vbox = new VBox(hbox, gridview);
-        vbox.setSpacing(5);// VBox to arrange the buttons and the Gridview vertically
-        stage.setScene(new Scene(vbox));
+        vbox.setSpacing(5);
+
+        // Bevestigingsmelding voor het schakelen naar de singleplayer-modus
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Switching to Singleplayer mode");
+        alert.setContentText("Are you sure you want to switch to Singleplayer mode?");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                stage.setScene(new Scene(vbox));
+            }
+        });
     }
+
     private void switchToMultiPlayerView() {
         Stage stage = (Stage) view.getScene().getWindow();
         Gridview gridviewP1 = new Gridview(stage);
         Gridview gridviewP2 = new Gridview(stage);
         new GridPresenter(new GridModel(), gridviewP1);
-        HBox hboxP1 = new HBox(gridviewP1.getShipTypeChoiceBox(), gridviewP1.getClearButton(), gridviewP1.getUndoButton(),gridviewP1.getNextButton());
+        HBox hboxP1 = new HBox(gridviewP1.getShipTypeChoiceBox(), gridviewP1.getClearButton(), gridviewP1.getUndoButton(), gridviewP1.getNextButton());
         hboxP1.setSpacing(10);
         VBox vboxP1 = new VBox(hboxP1, gridviewP1);
         vboxP1.setSpacing(5);
 
         new GridPresenter(new GridModel(), gridviewP2);
-        HBox hboxP2 = new HBox(gridviewP2.getShipTypeChoiceBox(), gridviewP2.getClearButton(), gridviewP2.getUndoButton(),gridviewP2.getNextButton());
+        HBox hboxP2 = new HBox(gridviewP2.getShipTypeChoiceBox(), gridviewP2.getClearButton(), gridviewP2.getUndoButton(), gridviewP2.getNextButton());
         hboxP2.setSpacing(10);
         VBox vboxP2 = new VBox(hboxP2, gridviewP2);
         vboxP2.setSpacing(5);
-        vboxP2.setVisible(false); // Initially, the Gridview for Player2 is not visible
+        vboxP2.setVisible(false);
 
-        // In BattleshipsPresenter.java
         gridviewP1.getNextButton().setOnAction(event -> {
             GridPresenter presenterP1 = new GridPresenter(new GridModel(), gridviewP1);
             if (presenterP1.allShipsPlaced()) {
                 vboxP1.setVisible(false);
                 vboxP2.setVisible(true);
-                stage.show(); // Update the view
+                stage.show();
             } else {
-                // Toon een bericht dat niet alle schepen zijn geplaatst
                 Alert alert = new Alert(Alert.AlertType.WARNING);
                 alert.setTitle("Warning");
                 alert.setHeaderText("Not all ships are placed");
@@ -121,8 +134,17 @@ public class BattleshipsPresenter {
             }
         });
 
-        VBox vbox = new VBox(vboxP1, vboxP2);
-        stage.setScene(new Scene(vbox));
-        stage.show();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Switching to Multiplayer mode");
+        alert.setContentText("Are you sure you want to switch to Multiplayer mode?");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                VBox vbox = new VBox(vboxP1, vboxP2);
+                stage.setScene(new Scene(vbox));
+                stage.show();
+            }
+        });
     }
 }
